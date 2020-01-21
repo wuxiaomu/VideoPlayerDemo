96330810700ac31ad7705ab7b71d322d2a193f67
wuxiaomu <41727922+wuxiaomu@users.noreply.github.com>
2020-01-21 20:22:31 +0800

    添加demo

---
+++ VideoPlayerDemo/Assets/Scripts/UIVideoPanel.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+[Serializable]
+public class FloatEvent : UnityEvent<float> { }
+
+public class UIVideoPanel : MonoBehaviour
+{
+
+    private VideoController controller;
+
+    [SerializeField]
+    private VideoClip clip;
+
+    [SerializeField]
+    private GameObject StartPanel;
+    [SerializeField]
+    private Button PlayBtn;
+
+    [SerializeField]
+    private Button PlayPauseBtn;
+    [SerializeField]
+    private Text PlayBtnTxt;
+
+    [SerializeField]
+    private Slider PositionSlider;
+    [SerializeField]
+    private Slider PreviewSlider;
+
+    [SerializeField]
+    private FloatEvent onSeeked = new FloatEvent();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<VideoController>();
+        PlayBtn.onClick.AddListener(PlayVideo);
+        PlayPauseBtn.onClick.AddListener(ToggleIsPlaying);
+        PositionSlider.onValueChanged.AddListener(SliderValueChanged);
+
+        StartPanel.SetActive(true);
+    }
+
+    private void OnDestroy()
+    {
+        PlayBtn.onClick.RemoveListener(PlayVideo);
+        PlayPauseBtn.onClick.RemoveListener(ToggleIsPlaying);
+        PositionSlider.onValueChanged.RemoveListener(SliderValueChanged);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (controller.IsPlaying)
+        {
+            PreviewSlider.value = controller.NormalizedTime;
+        }
+    }
+
+    private void PlayVideo()
+    {
+        if (clip == null)
+        {
+            return;
+        }
+        StartPanel.SetActive(false);
+        controller.PrepareForClip(clip);
+        PlayBtnTxt.text = "Pause";
+    }
+
+    private void ToggleIsPlaying()
+    {
+        if (controller.IsPlaying)
+        {
+            controller.Pause();
+            PlayBtnTxt.text = "Play";
+        }
+        else
+        {
+            controller.Play();
+            PlayBtnTxt.text = "Pause";
+        }
+    }
+
+    private void SliderValueChanged(float value)
+    {
+        onSeeked.Invoke(value);
+    }
+}
---
+++ VideoPlayerDemo/Assets/Scripts/VideoController.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Video;
+
+public class VideoController : MonoBehaviour
+{
+
+    [SerializeField]
+    private bool startAfterPreparation = true;
+
+    [Header("Optional")]
+
+    [SerializeField]
+    private VideoPlayer videoPlayer;
+
+    [SerializeField]
+    private AudioSource audioSource;
+
+    [Header("Events")]
+
+    [SerializeField]
+    private UnityEvent onPrepared = new UnityEvent();
+
+    [SerializeField]
+    private UnityEvent onStartedPlaying = new UnityEvent();
+
+    [SerializeField]
+    private UnityEvent onFinishedPlaying = new UnityEvent();
+
+    #region Properties
+
+    public bool StartAfterPreparation
+    {
+        get { return startAfterPreparation; }
+        set { startAfterPreparation = value; }
+    }
+
+    public UnityEvent OnPrepared
+    {
+        get { return onPrepared; }
+    }
+
+    public UnityEvent OnStartedPlaying
+    {
+        get { return onStartedPlaying; }
+    }
+
+    public UnityEvent OnFinishedPlaying
+    {
+        get { return onFinishedPlaying; }
+    }
+
+    public ulong Time
+    {
+        get { return (ulong)videoPlayer.time; }
+    }
+
+    public bool IsPlaying
+    {
+        get { return videoPlayer.isPlaying; }
+    }
+
+    public bool IsPrepared
+    {
+        get { return videoPlayer.isPrepared; }
+    }
+
+    public float NormalizedTime
+    {
+        get { return (float)(videoPlayer.time / Duration); }
+    }
+
+    public ulong Duration
+    {
+        get { return videoPlayer.frameCount / (ulong)videoPlayer.frameRate; }
+    }
+
+    public float Volume
+    {
+        get { return audioSource == null ? videoPlayer.GetDirectAudioVolume(0) : audioSource.volume; }
+        set
+        {
+            if (audioSource == null)
+                videoPlayer.SetDirectAudioVolume(0, value);
+            else
+                audioSource.volume = value;
+        }
+    }
+    #endregion
+
+    #region Unity Methods
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (videoPlayer == null)
+        {
+            SubscribeToVideoPlayerEvents();
+        }
+
+        videoPlayer.playOnAwake = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        SubscribeToVideoPlayerEvents();
+    }
+
+    private void OnDisable()
+    {
+        UnsubscribeFromVideoPlayerEvents();
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    public void PrepareForUrl(string url)
+    {
+        videoPlayer.source = VideoSource.Url;
+        videoPlayer.url = url;
+        videoPlayer.Prepare();
+    }
+
+    public void PrepareForClip(VideoClip clip)
+    {
+        videoPlayer.source = VideoSource.VideoClip;
+        videoPlayer.clip = clip;
+        videoPlayer.Prepare();
+    }
+
+    public void Play()
+    {
+        if (!IsPrepared)
+        {
+            videoPlayer.Prepare();
+            return;
+        }
+
+        videoPlayer.Play();
+    }
+
+    public void Pause()
+    {
+        videoPlayer.Pause();
+    }
+
+    public void TogglePlayPause()
+    {
+        if (IsPlaying)
+            Pause();
+        else
+            Play();
+    }
+
+    public void Seek(float time)
+    {
+        time = Mathf.Clamp(time, 0, 1);
+        videoPlayer.time = time * Duration;
+    }
+    #endregion
+
+    #region Private Methods
+
+    private void OnPrepareCompleted(VideoPlayer source)
+    {
+        onPrepared.Invoke();
+        SetupAudio();
+
+        if (StartAfterPreparation)
+            Play();
+    }
+
+    private void OnStarted(VideoPlayer source)
+    {
+        onStartedPlaying.Invoke();
+    }
+
+    private void OnFinished(VideoPlayer source)
+    {
+        onFinishedPlaying.Invoke();
+    }
+
+    private void OnError(VideoPlayer source, string message)
+    {
+        Debug.LogError("OnError " + message);
+    }
+
+    private void SetupAudio()
+    {
+        if (videoPlayer.audioTrackCount <= 0)
+            return;
+
+        if (audioSource == null && videoPlayer.canSetDirectAudioVolume)
+        {
+            videoPlayer.audioOutputMode = VideoAudioOutputMode.Direct;
+        }
+        else
+        {
+            videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
+            videoPlayer.SetTargetAudioSource(0, audioSource);
+        }
+        videoPlayer.controlledAudioTrackCount = 1;
+        videoPlayer.EnableAudioTrack(0, true);
+    }
+
+    private void SubscribeToVideoPlayerEvents()
+    {
+        if (videoPlayer == null)
+            return;
+
+        videoPlayer.errorReceived += OnError;
+        videoPlayer.prepareCompleted += OnPrepareCompleted;
+        videoPlayer.started += OnStarted;
+        videoPlayer.loopPointReached += OnFinished;
+    }
+
+    private void UnsubscribeFromVideoPlayerEvents()
+    {
+        if (videoPlayer == null)
+            return;
+
+        videoPlayer.errorReceived -= OnError;
+        videoPlayer.prepareCompleted -= OnPrepareCompleted;
+        videoPlayer.started -= OnStarted;
+        videoPlayer.loopPointReached -= OnFinished;
+    }
+    #endregion
+}
